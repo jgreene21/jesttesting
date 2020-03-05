6a80b26e20f41173cae4b1bce8cdc1d625acdfc6
Julie Greene <julie_greene22@yahoo.com>
2020-03-04 21:50:54 -0700

    progress on jest training

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,124 @@
 import {
-  originalArray 
+  originalArray,
+  doubleArray,
+  evensOnlyArray,
+  arraySum,
+  allNumbersGreaterThanZero,
+  someNumbersGreaterThanZero,
+  someNumbersAreOdd,
+  evensOnlyAndDoubleArray, 
+  findItem,
+  sortArray,
+  doubleLikes,
+  moreThan100Likes,
+  numberOfLikes,
+  sortArtistByName
 } from '../app'
+
+
+
 
 describe('original array test', () => {
   test('original array should return itself', () => {
-    expect(originalArray([1,2,3,4]).toEqual([1,2,3,4]))
+    expect(originalArray([1,2,3,4])).toEqual([1,2,3,4])
+  })
+})
+
+describe('testing double array', () => {
+  test('original array should double', () => {
+    expect(doubleArray([1,2,3,4])).toEqual([2,4,6,8])
+  })
+})
+
+describe('testing for even numbers', () => {
+  test('original array should return only even numbers', () => {
+    expect(evensOnlyArray([1,2,3,4])).toEqual([2,4])
+  })
+})
+
+describe('testing for sum', () => {
+  test('original array should return the sum', () => {
+    expect(arraySum([1,2,3,4])).toEqual(10)
+  })
+})
+
+describe('testing all numbers greater than zero', () => {
+  test('original array should return true', () => {
+    expect(allNumbersGreaterThanZero([1,2,3,4])).toEqual(true)
+  })
+})
+
+describe('testing some numbers greater than zero', () => {
+  test('original array should return true', () => {
+    expect(someNumbersGreaterThanZero([1,2,3,4])).toEqual(true)
+  })
+})
+
+describe('testing some numbers are odd', () => {
+  test('original array should return true', () => {
+    expect(someNumbersAreOdd([1,2,3,4])).toEqual(true)
+  })
+})
+
+describe('testing evens only and double array', () => {
+  test('original array should double and then return only evens', () => {
+    expect(evensOnlyAndDoubleArray([1,2,3,4])).toEqual([4,8])
+  })
+})
+
+describe('testing if not found method', () => {
+  test('original array should return not found', () => {
+    expect(findItem([1,2,3,4])).toEqual("not found")
+  })
+})
+
+describe('testing descending sort of array', () => {
+  test('original array should sort in descending order', () => {
+    expect(sortArray([1,2,3,4])).toEqual([4,3,2,1])
+  })
+})
+
+describe('testing doubling likes', () => {
+  test('original array should return double the likes', () => {
+    expect(doubleLikes([
+        {name: 'a', likes:100},
+        {name: 'c', likes:99},
+        {name: 'c', likes:101}])).toEqual([
+          {name: 'a', likes:200},
+          {name: 'c', likes:198},
+          {name: 'c', likes:202}])
+  })
+})
+
+describe('testing for more than 100 likes', () => {
+  test('original array should return double the likes', () => {
+    expect(moreThan100Likes([
+        {name: 'a', likes:100},
+        {name: 'c', likes:99},
+        {name: 'c', likes:101}])).toEqual(
+          [{name: 'c', likes:101}])
+  })
+})
+
+// describe('testing for more than 100 likes', () => {
+//   test('original array should return double the likes', () => {
+//     expect(numberOfLikes([
+//         {name: 'a', likes:100},
+//         {name: 'c', likes:99},
+//         {name: 'c', likes:101}])).toEqual(300)
+//   })
+// })
+
+
+describe('testing for sorting by likes', () => {
+  test('original array should sort by most likes to least', () => {
+    expect(moreThan100Likes([
+        {name: 'a', likes:100},
+        {name: 'c', likes:99},
+        {name: 'c', likes:101}])).toEqual(
+          [{name: 'c', likes:101},
+          {name: 'a', likes:100},
+          {name: 'c', likes:99}])
+        
   })
 })
--- app.js
+++ app.js
@@ -1,3 +1,104 @@
+// numberArray = [1,2,3,4]
+
 export const originalArray = (arr) => {
   return arr 
+}
+
+// return array with double values in array
+export const doubleArray = (arr) => {
+  return arr.map(num => {
+  return num * 2 
+})
+}
+// => [2,4,6,8];  hint use map 
+
+// // return array with all even numbers
+export const evensOnlyArray= (arr) => {
+  return arr.filter(num => {
+  return num % 2 === 0 
+  })
+  }
+ // => [2,4,6,8];  hint use filter 
+
+// // return array with sum of numbers
+export const arraySum =(arr) => {
+  return arr.reduce((accumulator, currentValue) =>  {
+  return accumulator + currentValue
+  })
+  }
+  // => 10;  hint use reduce
+
+// // return true if every item greater than 0
+export const allNumbersGreaterThanZero=(arr) => {
+  return arr.every((element, index, array) => {
+    return element >= 0 
+  })
+  }
+ // => true; use every
+
+// // return true if some item greater than 0
+export const someNumbersGreaterThanZero=(arr) => {
+  return arr.some((element, index, array) => {
+    return element >= 0
+  })
+}
+
+export const someNumbersAreOdd=(arr) => {
+  return arr.some((element, index, array) => {
+    return element % 2 !== 0 
+})
+} // => true; use some
+
+
+// // return array double and even
+export const evensOnlyAndDoubleArray=(arr) => {
+    const x = evensOnlyArray(arr) 
+    const y = doubleArray(x)
+    return y 
+}
+//[4,8] // use map and filter
+
+
+// // find an item method return 'not found' if not found
+export const findItem=(arr) => {
+  if (arr.indexOf("5") !== -1){
+    return("found")
+  } else {
+    return("not found")
+  }
+}
+
+export const sortArray=(arr) => {
+  return arr.sort((a,b) => {
+    return b-a
+  })
+}
+ //=> [4,3,2,1] *desc*
+
+// doubles likes of artist
+export const doubleLikes=(arr) => {
+  return arr.map((element) => {
+    return {name: element.name, likes: element.likes * 2} 
+  })
+}
+
+export const moreThan100Likes=(arr) => {
+  return arr.filter((element)=> {
+    return element.name, element.likes >100
+    })
+}
+
+// get total number of likes fo all artists
+// export const numberOfLikes=(artists) => {
+//   return artists.reduce((accumulator, currentValue, element) =>  {
+//     return accumulator + currentValue element.likes
+//     })
+//     }
+//=> 300; // get all likes
+
+// returns array of artist sorted by likes
+export const sortArtistByName=(arr) => {
+  return arr.sort((a,b) (element) => {
+    return {name: element.name, likes: element.likes (a-b)}
+  })
 }
